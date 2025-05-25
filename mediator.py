f96cb502fcd2be13a75db2e2bb62ba5e718d3bc2
Himalaya <himalaya.gupta3@gmail.com>
2025-05-25 13:35:37 +0530

    allowing ip and port

--- mediator.py
+++ mediator.py
@@ -6,13 +6,15 @@ from settings import INSTALLED_APPS
 from nublar.http_server import CoreHTTPServer
 import settings as app_settings  # to avoid name clash with your local 'settings'
 
-def run_server():
+def run_server(host=None, port=None):
     import app
     import nublar.app
-    HOST = getattr(app_settings, 'HOST', '127.0.0.1')
+
+    HOST = getattr(app_settings, 'HOST', '127.0.0.1') if not host else host
     if not HOST:
         HOST = '127.0.0.1'
-    PORT = getattr(app_settings, 'PORT', 4000)
+
+    PORT = getattr(app_settings, 'PORT', 4000) if not port else port
     if not PORT:
         PORT = 4000
     else:
@@ -40,7 +42,24 @@ def main():
     elif command == "migrate":
         migrate.run(INSTALLED_APPS)
     elif command == "runserver":
-        run_server()
+        host=None
+        port=None
+        if len(args) > 1:
+            if len(args) ==2:
+                if "--host=" in args[1]:
+                    host = args[1].split("=")[1]
+                if "--port=" in args[1]:
+                    port = int(args[1].split("=")[1])
+            if len(args) == 3:
+                if "--host=" in args[1]:
+                    host = args[1].split("=")[1]
+                if "--port=" in args[1]:
+                    port = int(args[1].split("=")[1])
+                if "--host=" in args[2]:
+                    host = args[2].split("=")[1]
+                if "--port=" in args[2]:
+                    port = int(args[2].split("=")[1])
+        run_server(host, port)
     else:
         print(f"Unknown command: {command}")
 
